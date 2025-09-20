6a0cbde3a1c1665984c7193ecc3ee395a87c857f
Zhendong Chen <chen86349@gmail.com>
2025-09-20 00:58:50 -0500

    Update

---
+++ Sandbox/Sandbox/PreviewViews/MistView.swift
@@ -0,0 +1,18 @@
+//
+//  MistView.swift
+//  Sandbox
+//
+//  Created by Zhendong Chen on 9/19/25.
+//
+
+import SwiftUI
+
+struct MistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MistView()
+}
